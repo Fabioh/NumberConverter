11bf7c60a15e8dd7676f01099c849eaef23e8e9c
Fábio Henrique Gabriele <fabiohenriqueg@gmail.com>
2020-10-12 13:50:01 -0300

    Add project files.

---
+++ src/NumberConverter.Core/DecimalToBinary.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Concurrent;
+using System.Text;
+
+namespace NumberConverter.Core
+{
+    public static class DecimalToBinary
+    {
+        public static string ConvertToString(uint decNumber)
+        {
+            var stack = new ConcurrentStack<string>();
+            var sb = new StringBuilder();
+            var initialNumber = decNumber;
+
+            while (initialNumber / 2 > 0)
+            {
+                stack.Push((initialNumber % 2).ToString());
+                initialNumber /= 2;
+            }
+
+            stack.Push((initialNumber % 2).ToString());
+
+            while (stack.TryPop(out var val))
+            {
+                sb.Append(val);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ tests/NumberConverter.Test/DecimalToBinaryTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using static NumberConverter.Core.DecimalToBinary;
+
+namespace NumberConverter.Test
+{
+    public class DecimalToBinaryTests
+    {
+        [InlineData(1, "1")]
+        [InlineData(2, "10")]
+        [InlineData(3, "11")]
+        [InlineData(5, "101")]
+        [InlineData(7, "111")]
+        [InlineData(33, "100001")]
+        [InlineData(50, "110010")]
+        [InlineData(72, "1001000")]
+        [InlineData(1024, "10000000000")]
+        [InlineData(2000, "11111010000")]
+        [Theory]
+        public void ConvertUint(uint decNumber, string exppected) =>
+            ConvertToString(decNumber)
+                .Should()
+                .Be(exppected);
+        
+    }
+}
