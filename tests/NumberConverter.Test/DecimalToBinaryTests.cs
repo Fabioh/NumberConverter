c1aca4fe66798226aece955a0b7e7fd9ab4aa4f7
Fábio Henrique Gabriele <Fabioh@users.noreply.github.com>
2020-10-12 19:48:31 -0300

    increase-code-coverage (#4)
    * - Increase code coverage
    
    * - fix tests

--- tests/NumberConverter.Test/DecimalToBinaryTests.cs
+++ tests/NumberConverter.Test/DecimalToBinaryTests.cs
@@ -7,7 +7,8 @@ namespace NumberConverter.Test
 {
     public class DecimalToBinaryTests
     {
-        private static readonly IDecimalToBinary _coverter = new MyDecimalToBinary();
+        private static readonly IDecimalToBinary _myConverter = new MyDecimalToBinary();
+        private static readonly IDecimalToBinary _systemConverter = new SystemDecimalToBinary();
 
         [InlineData(1, "1")]
         [InlineData(2, "10")]
@@ -20,8 +21,8 @@ namespace NumberConverter.Test
         [InlineData(1024, "10000000000")]
         [InlineData(2000, "11111010000")]
         [Theory]
-        public void Convert_Integer_To_Binary_String(uint decNumber, string exppected) =>
-            _coverter.ConvertToString(decNumber)
+        public void My_Convert_Integer_To_Binary_String(uint decNumber, string exppected) =>
+            _myConverter.ConvertToString(decNumber)
                 .Should()
                 .Be(exppected);
 
@@ -38,9 +39,25 @@ namespace NumberConverter.Test
         [Theory]
         public void Convert_Integer_To_Binary_String_Equals_System_Conversion(uint decNumber)
         {
-            _coverter.ConvertToString(decNumber)
+            _myConverter.ConvertToString(decNumber)
                 .Should()
                 .Be(Convert.ToString(decNumber, 2));
         }
+
+        [InlineData(1, "1")]
+        [InlineData(2, "10")]
+        [InlineData(3, "11")]
+        [InlineData(5, "101")]
+        [InlineData(7, "111")]
+        [InlineData(33, "100001")]
+        [InlineData(50, "110010")]
+        [InlineData(72, "1001000")]
+        [InlineData(1024, "10000000000")]
+        [InlineData(2000, "11111010000")]
+        [Theory]
+        public void System_Convert_Integer_To_Binary_String(uint decNumber, string exppected) =>
+            _systemConverter.ConvertToString(decNumber)
+                .Should()
+                .Be(exppected);
     }
 }
