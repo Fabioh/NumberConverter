5632d04691d854cdf0875e8fec0b184f756c9b54
Fábio Henrique Gabriele <Fabioh@users.noreply.github.com>
2020-10-12 19:27:26 -0300

    - Add code with no test coverage (#3)

---
+++ src/NumberConverter.Core/SystemDecimalToBinary.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace NumberConverter.Core
+{
+    public class SystemDecimalToBinary : IDecimalToBinary
+    {
+        public string ConvertToString(uint decNumber) =>
+            Convert.ToString(decNumber, 2);
+    }
+}
